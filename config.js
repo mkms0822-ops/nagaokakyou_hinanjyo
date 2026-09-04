ff76edea4e1a1efaa4d0bf2e8c216bf5afcf25f9
mkms0822-ops <m.k.m.s0822@gmail.com>
2026-09-04 12:59:21 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /* 接続・マスタ設定。GitHubには置くが、本番トークンは公開リポジトリに含めないこと。 */
 window.APP_CONFIG = {
   // GASをデプロイして得られる Web App URL に差し替える
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwZaM9eYEoo6SWeDEQUWTODqhx6cyAJNSSjEmnJhSTu3akh_HplaVOK0zTIkxeY5O1z/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxqb3hZZS05ciwfdHOzGR-FTRTj-wQqYzLncQ_JlORibTNSl_Q9j1azJqeXoVBqCfJ2wQ/exec',
   // GAS側 SHARED_TOKEN と一致させる（試作用。公開リポジトリでは秘匿）
   TOKEN: 'NAGAOKAKYOU_HINANJYO',
 
